dfbd1aa34e183e6b66c86eafac6fc35196300fb7
Asraful-code235 <asrafulislamcodes@gmail.com>
2024-06-19 05:23:35 +0600

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,24 @@ import CustomLayout from "@/components/provider/CustomLayout";
 export const metadata: Metadata = {
   title: "si3",
   description: "Creating Pathways For Diverse Voices Of the New Economy",
+  metadataBase: new URL("https://si3home-frontend.vercel.app"),
+  icons: ["favicon.ico", "/favicon.ico"],
+  openGraph: {
+    title: "si3",
+    type: "website",
+    locale: "en",
+    url: "https://si3home-frontend.vercel.app",
+    siteName: "si3",
+    description: "Creating Pathways For Diverse Voices Of the New Economy",
+    images: [
+      {
+        url: "https://si3home-frontend.vercel.app/favicon.ico",
+        width: 1200,
+        height: 630,
+        alt: "SI3 Home Page",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
