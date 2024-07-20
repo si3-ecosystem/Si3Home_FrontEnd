d12e5af630f38f188b71a0356d9a21acb209cf82
Nessrine88 <macherki.nessrine@yahoo.com>
2024-07-20 17:12:48 +0100

    change projecct id

--- utils/client.ts
+++ utils/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "next-sanity";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "38kwpfln"; // "pv8y60vp"
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "h4ttr3aq"; // "pv8y60vp" "38kwpfln"
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; // "production"
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-05-03";
 
