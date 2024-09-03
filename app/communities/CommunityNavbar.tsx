f62591d478ed633cd3e23d694e9fb3aacfb3c4bf
AbdulSalamAbro <macherki.nessrine@yahoo.com>
2024-09-03 10:21:53 +0100

    change the navbar

---
+++ app/communities/CommunityNavbar.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { motion, useAnimation, AnimatePresence } from "framer-motion";
+import Link from "next/link";
+import urlFor from "@/utils/urlFor";
+
+const CommunityNavbar = ({ utils }: any) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeDropdown, setActiveDropdown] = useState<string | null>(null);
+  const controls = useAnimation();
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleDropdown = (dropdownName: string) => {
+    setActiveDropdown(activeDropdown === dropdownName ? null : dropdownName);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        controls.start({ backgroundColor: "#f1b8c6" }); // pink color
+      } else {
+        controls.start({ backgroundColor: "#d3b4c2" }); // default color
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [controls]);
+
+  return (
+    <motion.nav
+      animate={controls}
+      initial={{ backgroundColor: "#d3b4c2" }} // initial background color
+      className="fixed w-full z-50 backdrop-filter backdrop-blur-lg shadow-lg transition-all duration-100"
+    >
+      <div className="flex justify-between items-center px-5 md:px-16 py-4">
+        <div className="lg:hidden">
+          <button
+            onClick={toggleMenu}
+            aria-label="Open Menu"
+            className="text-white"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
+        </div>
+        
+        <div className="hidden lg:flex items-center justify-center">
+          <Link href="/" className="text-white text-5xl font-bold uppercase">
+            <img
+              src={urlFor(utils?.logo?.asset).url()}
+              alt={utils?.logo?.alt}
+              className="w-24 h-12 "
+            />
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4 md:space-x-14 xl:space-x-36 font-medium">
+          <div
+            className="relative flex items-center gap-2 cursor-pointer"
+            onMouseEnter={() => handleDropdown("about")}
+            onMouseLeave={() => handleDropdown("about")}
+          >
+          
+          </div>
+          <div className="group">
+            <div className="flex items-center gap-2 cursor-pointer rounded-xl py-2 px-4 bg-[#C8BAFD] group-hover:bg-[#3C1FEF] text-black group-hover:text-white">
+              <Image
+                src={"/bell.png"}
+                alt="Bell Icon"
+                width={36}
+                height={36}
+                className="w-9 h-9"
+              />
+              <Link
+                href={utils?.stayConnected}
+                target="_blank"
+                className="text-base lg:text-xl font-medium leading-[2.5px] lg:leading-[3px]"
+              >
+                {utils.stayConnectedText || "SUBSCRIBE"}
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ x: "-100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "-100%" }}
+            transition={{
+              duration: 0.5,
+              ease: "easeInOut",
+            }}
+            className="mobileNav fixed z-[99999999] backdrop-blur-md top-0 left-0 h-screen w-3/4 px-6 py-4 flex flex-col justify-start items-center text-2xl gap-6 md:hidden"
+          >
+            <button
+              onClick={() => setIsOpen(false)}
+              aria-label="Close Menu"
+              className="text-white self-end top-4 mb-4"
+            >
+              <motion.svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                whileHover={{ rotate: 90 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </motion.svg>
+            </button>
+            <div className="group">
+              <div className="flex items-center gap-2 cursor-pointer rounded-xl py-2 px-4 bg-[#C8BAFD] group-hover:bg-[#3C1FEF] text-black group-hover:text-white">
+                <Image
+                  src={"/bell.png"}
+                  alt="Bell Icon"
+                  width={36}
+                  height={36}
+                  className="w-9 h-9"
+                />
+                <Link
+                  href={utils?.stayConnected}
+                  target="_blank"
+                  className="text-base lg:text-xl font-medium leading-[2.5px] lg:leading-[3px]"
+                >
+                  {utils.stayConnectedText || "SUBSCRIBE"}
+                </Link>
+              </div>
+            </div>
+           
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.nav>
+  );
+};
+
+export default CommunityNavbar;
---
+++ app/communities/layout.tsx
@@ -0,0 +1,132 @@
+import type { Metadata } from "next";
+
+import "../../app/globals.css";
+import Footer from "@/components/Footer";
+import CustomLayout from "@/components/provider/CustomLayout";
+import image from "../../app/logo.webp";
+
+import groq from "groq";
+import { client } from "@/utils/client";
+import urlFor from "@/utils/urlFor";
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import CommunityNavbar from "./CommunityNavbar";
+
+export const revalidate = 3600;
+
+export async function getSeoData() {
+  const query = groq`*[_type == 'utils'][0]`;
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+async function sharedMetaData(params: any) {
+  const settings = await getSeoData();
+  const seoLogoUrl = settings?.seoLogo
+    ? urlFor(settings?.seoLogo).url()
+    : "/icons/logo.webp";
+  return {
+    // enable this for resolving opengraph image
+    metadataBase: new URL("https://www.si3.space/"),
+    title: {
+      default: settings?.seoTitle || "si3",
+      template: "%s",
+    },
+
+    icons: {
+      icon: [
+        { rel: "icon", url: "/icons/favicon-16x16.png", sizes: "16x16" },
+        {
+          rel: "icon",
+          url: "/icons/favicon-16x16.png",
+          sizes: "16x16",
+        },
+        {
+          rel: "apple-touch-icon",
+          url: "/icons/favicon-16x16.png",
+          sizes: "16x16",
+        },
+        {
+          rel: "mask-icon",
+          url: "/icons/safari-pinned-tab.svg",
+          color: "#5bbad5",
+        },
+        { rel: "icon", url: "/icons/favicon-16x16.png", sizes: "16x16" },
+      ],
+    },
+    description:
+      settings?.overview ||
+      "Creating Pathways For Diverse Voices Of the New Economy",
+    keywords: ["si3", "si/her", "web3"],
+    authors: [{ name: "Asraful" }],
+    canonical: "https://www.si3.space",
+    openGraph: {
+      type: "website",
+      url: "https://www.si3.space",
+      title: settings?.seoTitle || "si3",
+      description:
+        settings?.overview ||
+        "Creating Pathways For Diverse Voices Of the New Economy",
+      images: [
+        {
+          url: "/icons/logo.webp",
+        },
+        {
+          url: image.src,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: settings?.seoTitle || "si3",
+      description:
+        settings?.overview ||
+        "Creating Pathways For Diverse Voices Of the New Economy",
+      images: ["https://www.si3.space/icons/logo.webp"],
+    },
+    robots: {
+      index: true,
+      follow: true,
+    },
+  };
+}
+
+export async function generateMetadata({ params }: any) {
+  return await sharedMetaData(params);
+}
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const utils = await getSeoData();
+  return (
+    <html lang="en">
+      <head>
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=clash-display@400&display=swap"
+          rel="stylesheet"
+        />
+          <link href="https://api.fontshare.com/v2/css?f[]=clash-display@400,500,600,700&display=swap" rel="stylesheet"></link>
+         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet"></link>
+         <link href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&display=swap" rel="stylesheet"></link>
+      </head>
+      <body className="scroll-smooth">
+        <script
+          type="text/javascript"
+          id="hs-script-loader"
+          async
+          defer
+          src="//js.hs-scripts.com/45396312.js"
+        ></script>
+        <CustomLayout>
+          <CommunityNavbar utils= {utils} />
+          <div className="min-h-screen">{children}</div>
+          <Footer />
+        </CustomLayout>
+      </body>
+    </html>
+  );
+}
