7bb3fb3f7094861f3d245d1ce8c8eb09d402afab
Asraful-code235 <asrafulislamcodes@gmail.com>
2024-12-06 00:00:48 +0600

    feat/redesign

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,6 +6,37 @@ import { motion, useAnimation, useInView } from "framer-motion";
 import Image from "next/image";
 import urlFor from "@/utils/urlFor";
 
+const partners = [
+  {
+    title: "ecosystem partners",
+    src: "/icons/Ecosystem Partners_.svg",
+  },
+  {
+    title: "livepeer",
+    src: "/icons/liepeer.svg",
+  },
+  {
+    title: "huddle",
+    src: "/icons/huddle.svg",
+  },
+  {
+    title: "pinata",
+    src: "/icons/pinata.svg",
+  },
+  {
+    title: "aurpay",
+    src: "/icons/aurpay.svg",
+  },
+  {
+    title: "charmverse",
+    src: "/icons/charmverse.svg",
+  },
+  {
+    title: "ikiguide",
+    src: "/icons/ikiguide.svg",
+  },
+];
+
 export default function Banner({ hero }: any) {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
@@ -24,31 +55,32 @@ export default function Banner({ hero }: any) {
   return (
     <motion.div
       ref={ref}
-      className="relative w-full h-[482px] md:h-screen overflow-hidden"
+      className="relative w-full max-md:h-[460px] max-h-[860px] -z-10 md:h-screen overflow-hidden  bg-gradient-to-r from-[#F5B6D3] to-[#E5B9DA]"
     >
       <div className="absolute inset-0 -z-40 ">
         <Image
-          src="/bgBanner_mobile.webp"
+          src="/heromobile.png"
           alt="Background image mobile"
           width={800}
           height={500}
           priority
-          className="md:hidden w-full translate-x-2 scale-[1.5]"
+          className="md:hidden w-full translate-x-36 scale-[1.2] translate-y-20 object-cover"
         />
         <motion.div className="hidden md:block">
           <Image
-            src="/banner.webp"
+            src="/herobgdesktop.png"
             alt="Background image desktop"
             layout="fill"
             decoding="async"
             objectFit="cover"
             priority
-            objectPosition="right center"
+            objectPosition="center right "
+            className="ml-80  mt-20"
           />
         </motion.div>
       </div>
       <div className="absolute inset-0" />
-      <div className="relative z-10 px-10 md:px-16 pt-36 2xl:pt-[196px] ">
+      <div className="relative z-10 px-4 md:px-16 pt-36 2xl:pt-[196px] ">
         <motion.h1
           className="text-white text-2xl md:text-6xl font-[1000]"
           initial={{ opacity: 0.7, x: 500 }}
@@ -64,28 +96,26 @@ export default function Banner({ hero }: any) {
           {hero?.headingTwo}
         </motion.h2>
         <motion.p
-          className="md:pt-6 text-white text-sm md:text-xl font-mono max-w-xs max-sm:mt-4  "
+          className="md:pt-6 text-[#454545] text-sm md:text-xl font-mono max-w-[182px] md:max-w-[522px] max-sm:mt-4  "
           initial={{ opacity: 0.7, x: 500 }}
           animate={controls}
         >
           {hero?.subHeader}
         </motion.p>
-        <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
-          transition={{
-            duration: 0.5,
-            ease: "easeInOut",
-          }}
-          className="mt-8 md:mt-10 xl:mt-[80px] flex items-center justify-center w-full md:w-fit"
-        >
-          <a href={hero.cta.link.external} target="_blank" className="w-full">
-            <ButtonCTA variant="outline">{hero?.cta?.text}</ButtonCTA>
-          </a>
-        </motion.div>
+      </div>
+      {/* banner */}
+      <div className="bg-[#F0DEDE] h-[100px] w-full absolute bottom-0 z-40  hidden md:flex  items-center justify-between gap-2 px-10 md:px-16">
+        {partners.map((item, key) => (
+          <div key={key}>
+            <Image
+              src={item.src}
+              alt={item.title}
+              width={400}
+              height={400}
+              className="w-full h-auto aspect-auto"
+            />
+          </div>
+        ))}
       </div>
     </motion.div>
   );
