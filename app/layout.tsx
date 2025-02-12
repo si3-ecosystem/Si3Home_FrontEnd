127369fb5370b6c9affa2194d44026c5d870439f
Hassan-124545 <hr84177@gmail.com>
2025-02-13 04:22:56 +0500

    updated react versions

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ import Script from "next/script";
 export const revalidate = 3600;
 
 export async function getSeoData() {
-  const query = groq`*[_type == "utils][0]`;
+  const query = groq`*[_type == "utils"][0]`;
   const data = await client.fetch(query);
 
   return data;
