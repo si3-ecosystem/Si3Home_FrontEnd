1231d6f77e4271136e5438027289e87b17097035
Goodluck Edih <goodluckedih@yahoo.com>
2025-02-14 19:24:31 +0100

    fixed deployment issues

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
       },
     ],
   },
+  env:{
+    NEXT_PUBLIC_SANITY_PROJECT_ID:"h4ttr3aq",
+    NEXT_PUBLIC_SANITY_DATASET:"production",
+    NEXT_PUBLIC_SANITY_API_VERSION:"2023-05-03",
+    NEXT_PUBLIC_SANITY_PROJECT_ID_2:"h4ttr3aq",
+    NEXT_PUBLIC_SANITY_DATASET_2:"production",
+    NEXT_PUBLIC_SANITY_API_TOKEN:"skGojtvtGu5GRPeLuBo5XUVM347KQTcpPHsTtR0q1pagvLlEbg0AUWMCYFCc3iwIbX1gCg7ymyzdXcs0eaMgGbHb3DVdviIF17XrqE6fHEo4kDtQow0GtRQMGrL1YuPcDFbYgGawYwdsmOr8iWoA6LPjJcZCkYSSujnreREgTGriclMtuvm8"
+
+  }
 };
 
 export default nextConfig;
